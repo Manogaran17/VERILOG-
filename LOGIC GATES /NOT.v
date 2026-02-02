acf45d857ed4488eacfd10a530cb2f8975dd1c6a
Manogaran17 <manogaran2667@gmail.com>
2026-02-03 00:26:16 +0530

    Update NOT.v

--- LOGIC GATES /NOT.v
+++ LOGIC GATES /NOT.v
@@ -4,3 +4,20 @@ module not_struc(a,y);
   output y;
   not g1(y,a);
 endmodule
+
+// test bench
+
+
+module not_tb;
+  reg a;
+  wire y;
+  not_struc dut(a,y);
+  initial 
+    begin 
+      $monitor("Time=%0t,a=%b,y=%b", $time, a, y);
+      a=0;#10;
+      a=1;#10;
+     
+      $finish;
+    end
+endmodule
