9d574908fb2d1d26fe7ee8d1068a0a16383f5dc0
Manogaran17 <manogaran2667@gmail.com>
2026-02-03 23:43:35 +0530

    Update and rename 4_full subtractor to 4_full subtractor.v

---
+++ 1_combinational-1/4_full subtractor.v
@@ -0,0 +1,31 @@
+
+module full_sub_dataflow(a,b,bin,D,B);
+  input a,b,bin;
+  output D,B;
+  assign D=(a^b^bin);
+  assign B=(~a&bin)|(~a&b)|(b&bin);
+endmodule 
+
+
+// test bench
+
+module FS_tb;
+  reg a,b,bin;
+  wire D,B;
+  
+  full_sub_dataflow dut(a,b,bin,D,B);
+  initial 
+    begin
+      $monitor("time=%0t,a=%b,b=%b,bin=%b,B=%b,D=%b",$time,a,b,bin,D,B);
+     a=0;b=0;bin=0;#10;
+     a=0;b=0;bin=1;#10;
+     a=0;b=1;bin=0;#10;
+     a=0;b=1;bin=1;#10;
+     a=1;b=0;bin=0;#10;
+     a=1;b=0;bin=1;#10;
+     a=1;b=1;bin=0;#10;
+     a=1;b=1;bin=1;#10;
+      $finish;
+    end 
+endmodule 
+      
