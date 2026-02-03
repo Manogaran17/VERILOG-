ecd60d29471a1a7d10a22fd290bb5a12273e4c68
Manogaran17 <manogaran2667@gmail.com>
2026-02-04 00:00:59 +0530

    Update and rename  OR GATE.v to  AND.v

---
+++ 1_Logic gates/ AND.v
@@ -0,0 +1,23 @@
+// Code your design here
+// Code your design here
+module and_struc(a,b,y);
+  input a,b;
+  output y;
+  and g1(y,a,b);
+endmodule
+// Code your testbench here
+
+module and_tb;
+  reg a,b;
+  wire y;
+  and_struc dut(a,b,y);
+  initial 
+    begin 
+    $monitor("Time=%0t,a=%b b=%b,y=%b", $time, a, b, y);
+      a=0;b=0;#10;
+      a=0;b=1;#10;
+      a=1;b=0;#10;
+      a=1;b=1;#10;
+      $finish;
+    end
+endmodule
