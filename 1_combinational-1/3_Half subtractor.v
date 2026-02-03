5f29344aa885612f88772502a7826d13f30101d2
Manogaran17 <manogaran2667@gmail.com>
2026-02-03 23:44:54 +0530

    Update 3_Half subtractor.v

--- 1_combinational-1/3_Half subtractor.v
+++ 1_combinational-1/3_Half subtractor.v
@@ -1,32 +1,25 @@
-
-module full_sub_struc(a,b,bin,D,B);
-  input a,b,bin;
+module HA_dataflow(a,b,D,B);
+  input a,b;
   output D,B;
-  assign D=(a^b^bin);
-  assign B=(~a&bin)|(~a&b)|(b&bin);
-endmodule 
+ assign D=(a^b);
+  assign B=((~a)&b);
+endmodule
 
+//test bench 
 // Code your testbench here
 // or browse Examples
-
-module FS_tb;
-  reg a,b,bin;
+module HS_tb;
+  reg a,b;
   wire D,B;
-  
-  full_sub_struc dut(a,b,bin,D,B);
-  initial 
-    begin
-      $monitor("time=%0t,a=%b,b=%b,bin=%b,B=%b,D=%b",$time,a,b,bin,D,B);
-     a=0;b=0;bin=0;#10;
-     a=0;b=0;bin=1;#10;
-     a=0;b=1;bin=0;#10;
-     a=0;b=1;bin=1;#10;
-     a=1;b=0;bin=0;#10;
-     a=1;b=0;bin=1;#10;
-     a=1;b=1;bin=0;#10;
-     a=1;b=1;bin=1;#10;
+  HA_dataflow dut (a,b,D,B);
+    initial
+      begin 
+        $monitor("time = %0t,a=%b,b=%b,D=%b,B=%b",$time,a,b,D,B);       
+     a=0;b=0;#10;
+     a=0;b=1;#10;
+     a=1;b=0;#10;           
+     a=1;b=1;#10;
+             
       $finish;
-    end 
-endmodule 
-      
-  
+    end
+endmodule
