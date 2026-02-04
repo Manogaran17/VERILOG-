f7cea25a7bffbd41b50380af7d7abc8e4a19a2bc
Manogaran17 <manogaran2667@gmail.com>
2026-02-04 11:58:48 +0530

    Create 6_Full adder behavioural.v

---
+++ 1_combinational-1/6_Full adder behavioural.v
@@ -0,0 +1,30 @@
+module full_adder_dataflow(a,b,cin,sum,cout);
+  input a,b,cin;
+  output reg sum,cout;
+  always@(*)
+    begin 
+  sum = (a^b^cin);
+  cout = (a&b)|cin&(a^b);
+  end 
+endmodule 
+
+// test bench 
+module hd_tb;
+  reg a,b,cin;
+  reg sum,cout;
+  full_adder_dataflow dut(a,b,cin,sum,cout);
+  initial 
+    begin 
+      $monitor("Time=%0t,a=%b b=%b,cin=%b,sum=%b,cout=%b", $time, a, b, cin, sum, cout);
+      a=0;b=0;cin=0;#10;
+      a=0;b=0;cin=1;#10;
+     a=0;b=1;cin=0;#10;
+     a=0;b=1;cin=1;#10;
+     a=1;b=0;cin=0;#10;
+     a=1;b=0;cin=1;#10;
+     a=1;b=1;cin=0;#10;
+     a=1;b=1;cin=1;#10;
+    
+      $finish;
+    end
+endmodule
