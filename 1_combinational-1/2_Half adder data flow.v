b4a1ddf6eb142a4162fc80a5f901d4b389a7a7ea
Manogaran17 <manogaran2667@gmail.com>
2026-02-05 09:19:02 +0530

    Update 2_Half adder data flow.v

--- 1_combinational-1/2_Half adder data flow.v
+++ 1_combinational-1/2_Half adder data flow.v
@@ -6,3 +6,18 @@ module ha(a,b,s,cout);
     end 
   endmodule
 
+// test bench  
+module ha_tb;
+  reg a,b;
+  wire s,cout;
+  ha dut(a,b,s,cout);
+  initial 
+    begin 
+      $monitor("Time=%0t,a=%b b=%b,s=%b,cout=%b", $time, a, b, s ,cout);
+      a=0;b=0;#10;
+      a=0;b=1;#10;
+      a=1;b=0;#10;
+      a=1;b=1;#10;
+      $finish;
+    end
+endmodule
