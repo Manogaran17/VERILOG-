14bdb74e4c5892466f875e7b58049c7cf4dbc53c
Manogaran17 <manogaran2667@gmail.com>
2026-02-05 09:25:13 +0530

    Update 3-1:8 DEMUX.v

--- 2_combinational-2/3-1:8 DEMUX.v
+++ 2_combinational-2/3-1:8 DEMUX.v
@@ -12,33 +12,26 @@ module demux1to8_struct (
     not g2(s2_n, s2);
 
    
-    and g3(y0, d, s2_n, s1_n, s0_n);
-    and g4(y1, d, s2_n, s1_n, s0);
-    and g5(y2, d, s2_n, s1,   s0_n);
-    and g6(y3, d, s2_n, s1,   s0);
-    and g7(y4, d, s2,   s1_n, s0_n);
-    and g8(y5, d, s2,   s1_n, s0);
-    and g9(y6, d, s2,   s1,   s0_n);
-    and g10(y7, d, s2,  s1,   s0);
+    and g3(y0, d, s2_n, s1_n,s0_n);
+    and g4(y1, d, s2_n, s1_n,s0);
+    and g5(y2, d, s2_n, s1, s0_n);
+    and g6(y3, d, s2_n, s1, s0);
+    and g7(y4, d, s2, s1_n, s0_n);
+    and g8(y5, d, s2, s1_n, s0);
+    and g9(y6, d, s2, s1, s0_n);
+    and g10(y7, d, s2, s1, s0);
 
 endmodule
 // test bench 
-
-// Code your testbench here
-// or browse Examples
-
-
 module demux1to8_struct_tb;
-
     reg d;
     reg s0, s1, s2;
     wire y0,y1,y2,y3,y4,y5,y6,y7;
 
-    
-  demux1to8_struct dut (d,s0,s1,s2,y0,y1,y2,y3,y4,y5,y6,y7);
+     demux1to8_struct dut (d,s0,s1,s2,y0,y1,y2,y3,y4,y5,y6,y7);
 
     initial begin
-        $monitor("Time=%0t | s2=%b s1=%b s0=%b | y0=%b y1=%b y2=%b y3=%b y4=%b y5=%b y6=%b y7=%b",
+        $monitor("Time=%0t ,s2=%b s1=%b s0=%b ,y0=%b y1=%b y2=%b y3=%b y4=%b y5=%b y6=%b y7=%b",
                  $time, s2,s1,s0,y0,y1,y2,y3,y4,y5,y6,y7);
 
         d = 1;
