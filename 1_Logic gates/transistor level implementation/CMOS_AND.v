34d64cae16a5db11338468a3e041e3f489492893
Manogaran17 <manogaran2667@gmail.com>
2026-02-08 12:29:27 +0530

    Update CMOS_AND.v

--- 1_Logic gates/transistor level implementation/CMOS_AND.v
+++ 1_Logic gates/transistor level implementation/CMOS_AND.v
@@ -18,7 +18,32 @@ module CMOS_and(a,b,y);
   nmos i1(y,gnd,w1);
   
 endmodule
+  // Code your testbench here
+// or browse Examples
+
+module CMOS_tb;
+  reg a,b;
+  wire y;
   
+  CMOS_and dut(a,b,y);
+  initial 
+    begin 
+      $dumpfile("cmos.vcd");
+      $dumpvars(1,CMOS_tb);
+    end 
+  
+  initial 
+    begin 
+      $monitor("time=%0t,a=%b,b=%b,y=%b",$time,a,b,y);
+      
+       a=0;b=0;#10;
+       a=0;b=1;#10;
+       a=1;b=0;#10;
+       a=1;b=1;#10;
+      #50$finish;
+    end 
+endmodule 
+       
  
   
   
