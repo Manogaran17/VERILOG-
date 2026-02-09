c450cf6ffadbb14910fd04c8e99956fbdaf66cfb
Manogaran17 <manogaran2667@gmail.com>
2026-02-09 16:09:48 +0530

    Create  SR_nand_latch active high.v

---
+++ 3_sequential circuit - 1/Latches/SR_latch/SR_nand_latch active high.v
@@ -0,0 +1,46 @@
+// Code your design here
+// Code your design here
+module sr_nandLatch(s,r,clk,q);
+  input s,r,clk;
+  output reg q;
+  
+  always@(clk or s or r)
+    begin
+      if(clk)    // active high 
+      case({s,r})
+        2'b00: q <=1'bX;
+        2'b01: q <=1;
+        2'b10: q <=0;
+        2'b11: q <=q;
+        default :q <=0; 
+      endcase 
+    end
+endmodule
+
+ module sr_nandLatch_tb;
+   reg s,r,clk;
+   wire q;
+     
+    initial begin
+    clk = 0;
+    forever #5 clk = ~clk;
+  end  
+     initial
+    begin
+    $dumpfile("dump.vcd");
+      $dumpvars(0, sr_nandLatch_tb);
+  end 
+   sr_nandLatch dut(s,r,clk,q);
+ initial begin 
+   $monitor("time=%0t,s=%b,r=%b,clk=%b,q=%b",$time,s,r,clk,q);
+       
+     s = 0; r = 0;
+    #12 s = 0; r = 1;
+    #18 s = 1; r = 0;
+    #12 s = 1; r = 1;
+    #5  s = 0; r = 0;
+   
+    #10 $finish;
+      
+     end 
+ endmodule
