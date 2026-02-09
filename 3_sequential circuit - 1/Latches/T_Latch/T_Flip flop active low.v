cbd5ac2456ed881436d222e1b28979922dc65292
Manogaran17 <manogaran2667@gmail.com>
2026-02-09 19:22:06 +0530

    Create T_Flip flop active low.v

---
+++ 3_sequential circuit - 1/Latches/T_Latch/T_Flip flop active low.v
@@ -0,0 +1,54 @@
+// Code your design here
+module t_latch (
+    input  t,
+    input  clk,
+    input  rst,
+    output reg q
+);
+
+always @(*) begin
+    if (rst)
+        q = 1'b0;
+  else if (!clk && t)
+        q = ~q;
+end
+
+endmodule
+
+module t_latch_tb;
+
+reg t, clk, rst;
+wire q;
+
+t_latch dut(t, clk, rst, q);
+
+initial begin
+    clk = 0;
+    forever #5 clk = ~clk;
+end
+
+initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars(0, t_latch_tb);
+end
+
+initial begin
+    rst = 1;
+    t   = 0;
+
+    #12 rst = 0;
+
+    #12 t = 1; 
+    #18 t = 0; 
+    #12 t = 1;
+    #12 t = 1; 
+    #18 t = 0; 
+    #12 t = 1;
+
+    #20 rst = 1;
+    #10 rst = 0;
+
+    #20 $finish;
+end
+
+endmodule
