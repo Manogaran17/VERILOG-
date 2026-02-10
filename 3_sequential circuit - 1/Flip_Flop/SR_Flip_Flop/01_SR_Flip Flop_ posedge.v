5beed2e95536f9a55a157b6f46caa4f84afe98e1
Manogaran17 <manogaran2667@gmail.com>
2026-02-10 23:41:05 +0530

    Update 01_SR_Flip Flop_ posedge.v

--- 3_sequential circuit - 1/Flip_Flop/SR_Flip_Flop/01_SR_Flip Flop_ posedge.v
+++ 3_sequential circuit - 1/Flip_Flop/SR_Flip_Flop/01_SR_Flip Flop_ posedge.v
@@ -1,24 +1,24 @@
 module sr_flipflop(s,r,clk,q);
   input s,r,clk;
-  output reg q;
-  
+  output reg q;  
   always@(posedge clk)
     begin
       case({s,r})
         2'b00: q <=q;
         2'b01: q <=0;
         2'b10: q <=1;
-        2'b11: q <=1'bX;
+        2'b11: q <=1'bx;
+        default: q<=0;
       endcase
     end
 endmodule
-// test bench 
+// Code your testbench here 
  module sr_flipflop_tb;
    reg s,r,clk;
    wire q;
      
     initial begin
-    clk = 0;
+    clk = 1;
     forever #5 clk = ~clk;
   end  
      initial
@@ -28,8 +28,13 @@ endmodule
   end 
    sr_flipflop dut(s,r,clk,q);
  initial begin 
-       
-     s = 0; r = 0;
+   $monitor("time=%t,s=%b,r=%b,clk=%b,q=%b",$time,s,r,clk,q);
+     
+     s = 1; r = 0;
+     #12 s = 0; r = 1;
+    #18 s = 1; r = 0;
+     #12 s = 1; r = 1;
+   
     #12 s = 0; r = 1;
     #18 s = 1; r = 0;
     #12 s = 1; r = 1;
