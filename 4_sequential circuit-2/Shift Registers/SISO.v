28ae97abcf92241636395535ecd98e677fe46d31
Manogaran17 <manogaran2667@gmail.com>
2026-02-15 23:31:59 +0530

    Update SISO.v

--- 4_sequential circuit-2/Shift Registers/SISO.v
+++ 4_sequential circuit-2/Shift Registers/SISO.v
@@ -13,6 +13,9 @@ module shift_register(s_in,clk,rst,s_out);
   assign s_out = regis[3];
 endmodule 
 
+
+
+
  module shift_register_tb;
   reg s_in,clk,rst;
   reg[ 3:0]regis;
@@ -23,25 +26,22 @@ endmodule
       clk=1;
   forever #5 clk=~clk;
     end 
-      
-   
-   shift_register dut (s_in,clk,rst,s_out);
+shift_register dut (s_in,clk,rst,s_out);
   initial 
     begin 
       $monitor("time=%0t,s_in=%b,clk=%b,rst=%b,s_out=%b",$time,s_in,clk,rst,s_out);
       
       rst=1; s_in = 0;
       #10rst=0;
-      
+        s_in = 1;#10;
+        s_in =0;#10;
+        s_in = 1;#10;
+        s_in =1;#10;  
+        s_in = 1;#10;
         s_in = 1; #10;
-        s_in = 0; #10;
-        s_in = 1; #10;
-        s_in = 1; #10;  
-        s_in = 1; #10;
-        s_in = 1; #10;
-        s_in = 0; #10;
-        s_in = 1; #10;
-        s_in = 0; #40;
+        s_in =0; #10;
+        s_in =1;#10;
+        s_in = 0;#40;
       
       
       #70$finish;
