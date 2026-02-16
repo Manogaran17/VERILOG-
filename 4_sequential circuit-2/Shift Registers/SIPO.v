9342c2df9a97b618713d3016d45b0701b5f0f3de
Manogaran17 <manogaran2667@gmail.com>
2026-02-16 10:36:57 +0530

    Update SIPO.v

--- 4_sequential circuit-2/Shift Registers/SIPO.v
+++ 4_sequential circuit-2/Shift Registers/SIPO.v
@@ -1,6 +1,6 @@
-module shift_register(s_in,clk,rst,s_out);
+module shift_register_SIPO(s_in,clk,rst,p_out);
   input s_in,clk,rst;
-  output [3:0]s_out;
+  output [3:0]p_out;
   reg [3:0]regis;
   
   always@(posedge clk)
@@ -10,9 +10,14 @@ module shift_register(s_in,clk,rst,s_out);
        else
            regis<={regis[2:0],s_in};      
     end 
-  assign s_out = regis[3:0];
-endmodule
+  assign p_out = regis[3:0];
+endmodule 
 
+
+
+
+// Code your testbench here
+// or browse Examples
 
 
 // Code your testbench here
@@ -22,19 +27,19 @@ endmodule
  module shift_register_tb;
   reg s_in,clk,rst;
   reg[ 3:0]regis;
-   wire [3:0]s_out;
+   wire [3:0]p_out;
   
   initial
     begin 
       clk=1;
   forever #5 clk=~clk;
     end 
-shift_register dut (s_in,clk,rst,s_out);
+   shift_register_SIPO dut (s_in,clk,rst,p_out);
   initial 
     begin 
-      $monitor("time=%0t,s_in=%b,clk=%b,rst=%b,s_out=%b",$time,s_in,clk,rst,s_out);    
+      $monitor("time=%0t,s_in=%b,clk=%b,rst=%b,p_out=%b",$time,s_in,clk,rst,p_out);    
       rst=1; s_in = 0;
-      #10rst=0;
+      #10 rst=0;
         s_in = 1;#10;
          s_in =0;#10;
         s_in = 1;#10;
