09b55d25e654900739da9ca63503c6ab750705c3
Manogaran17 <manogaran2667@gmail.com>
2026-02-21 18:37:16 +0530

    Update 1. Overlapping .v

--- 4_sequential circuit-2/FSM/Sequence detector /Melay Machine/1. Overlapping .v
+++ 4_sequential circuit-2/FSM/Sequence detector /Melay Machine/1. Overlapping .v
@@ -1,3 +1,50 @@
+module sequence_detector(x,clk,rst,y);
+  input x,clk,rst;
+  output reg y;
+  
+  parameter[1:0] a=0,b=1,c=2,d=3;
+  reg [1:0] state,nxt_state;
+  
+  always@(posedge clk)
+    begin 
+      if(rst)
+        state<=a;
+      else 
+        state<=nxt_state;
+    end 
+  always@(*)
+    begin 
+      if(!x && state==a)
+        nxt_state=a;
+      else if (x && state==a)
+        nxt_state=b;
+      else if (!x && state==b)
+        nxt_state=c;
+      else if (x && state==b)
+        nxt_state=b;
+      else if (!x && state==c)
+        nxt_state=d;
+      else if (x && state==c)
+        nxt_state=b;
+      else if (!x && state==d)
+        nxt_state=a;
+      else if (x && state==d)
+        nxt_state=b;
+      else
+        nxt_state=a;
+    end 
+  
+  always@(posedge clk)
+    begin 
+      if(rst)
+        y<=0;
+      else 
+      y<=(state==d && x==1);
+    end 
+    
+    endmodule 
+
+
 // Code your testbench here
 // or browse Examples
 
