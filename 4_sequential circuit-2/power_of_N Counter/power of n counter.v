8405ab6be70875d0d85255fef4282abad228eef6
Manogaran17 <manogaran2667@gmail.com>
2026-02-28 12:00:33 +0530

    Update power of n counter.v

--- 4_sequential circuit-2/power_of_N Counter/power of n counter.v
+++ 4_sequential circuit-2/power_of_N Counter/power of n counter.v
@@ -1,7 +1,8 @@
 // Code your design here
 
-module power_of_3counter#(parameter N=32,N1=10)(rst,clk,out);
+module power_of_3counter#(parameter N=16)(N1,rst,clk,out);
   input rst,clk;
+  input [3:0]N1;
   output reg [N-1:0]out;
   
   
@@ -10,8 +11,11 @@ module power_of_3counter#(parameter N=32,N1=10)(rst,clk,out);
       if(rst)
         out<=1;
       else 
-        out<=out*N1;
+        out <= out*N1;
     end
+  always@(N1)
+    out<=1;
+    
 endmodule 
   
   // Code your testbench here
@@ -19,8 +23,9 @@ endmodule
 
 module counter_tb;
   
-  parameter N = 16,N1=10;
+  parameter N = 16;
   reg rst,clk;
+  reg [3:0]N1;
   wire [N-1:0]out;
   initial 
     begin 
@@ -28,15 +33,22 @@ module counter_tb;
       forever #5 clk=~clk;
     end 
   
-  power_of_3counter #(N,N1) dut(rst,clk,out);
+  power_of_3counter #(N) dut(N1,rst,clk,out);
   initial 
     begin 
-      $monitor("time=%0t,rst=%b,clk=%b,out=%b",$time,rst,clk,out);
+      $monitor("time=%0t,N1=%b,rst=%b,clk=%b,out=%b",$time,N1,rst,clk,out);
       
-      rst=1;
+      rst=1;N1=1;
       #10 rst=0;
+       #10 N1=2;
+       #50 N1=3;
+       #50 N1=4;
+       #50 N1=5;
+      #50 N1=6;
+       #50 N1=7;
+       #50 N1=8;
       
-      #80$finish;
+      #10$finish;
     end 
   initial 
     begin 
@@ -50,5 +62,6 @@ endmodule
     
       
      
+  
   
   
